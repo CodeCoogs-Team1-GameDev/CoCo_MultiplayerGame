1e31f62b3929187479f6dc926530646207a48255
Sadison <memshousen@gmail.com>
2023-11-15 14:58:07 -0600

    speedPowerUp.cs added

---
+++ speedPowerUp.cs
@@ -0,0 +1,28 @@
+ // StrengthPowerUp.cs
+
+using UnityEngine;
+
+public class StrengthPowerUp : MonoBehaviour
+{
+    public float speedMultiplier = 4f; // Adjust this value based on your requirements
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ApplyPowerUp(other.gameObject);
+            Destroy(gameObject); // Destroy the power-up object once collected
+        }
+    }
+
+    void ApplyPowerUp(GameObject player)
+    {
+        // Apply strength power-up to the player
+        PlayerController playerController = player.GetComponent<PlayerController>();
+
+        if (playerController != null)
+        {
+            playerController.ApplySpeedpowerUp(strengthMultiplier);
+        }
+    }
+}
