1f63e113cd70c3f62bf67d44adc8c196018748f7
Sadison <memshousen@gmail.com>
2023-11-15 15:16:44 -0600

    Multiplayer Functionality V1
    My attempt at 2 player functionality

--- playerController.cs
+++ playerController.cs
@@ -4,7 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        // Handle player input and movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontal, 0, vertical) * Time.deltaTime * speed);
+
+        // Check for actions (e.g., shooting) and use [Command] to execute on the server
+        if (Input.GetButtonDown("Fire1"))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // Spawn bullets or perform shooting logic on the server
+        // Then use [ClientRpc] to notify clients about the action
+        RpcSpawnBullet();
+    }
+
+    [ClientRpc]
+    void RpcSpawnBullet()
+    {
+        // Spawn bullets on all clients
+    }
+    
     private float baseStrength = 1f;
+    private float baseSpeed = 1fl
     private float currentStrength;
 
     // Other player controller code...
